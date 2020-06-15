98a9b02aa6cb6b37a71ee7cd0d742e5cb1a24506
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-06-15 14:38:50 -0700

    created test route for auth signup using reference from demo and with gold room peeps

---
+++ __tests__/auth.test.js
@@ -0,0 +1,38 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongod = new MongoMemoryServer();
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
+
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('auth routes', () => {
+  beforeAll(async() => {
+    const uri = await mongod.getUri();
+    return connect(uri);
+  });
+  
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+  
+  afterAll(async() => {
+    await mongoose.connection.close();
+    return mongod.stop();
+  });
+  
+  it('signs up a user via POST /signup', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'breeanntest@breeanntest.com',
+        password: 'password1234'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          email: 'breeanntest@breeanntest.com'
+        });
+      });
+  });
+});
