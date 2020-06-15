0eef8f8b04bfa8a1e3e5a6f0235216b63f79e87e
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-06-15 15:33:20 -0700

    created test for get auction by id

--- __tests__/auction.test.js
+++ __tests__/auction.test.js
@@ -3,6 +3,7 @@ const mongod = new MongoMemoryServer();
 const mongoose = require('mongoose');
 const connect = require('../lib/utils/connect');
 const User = require('../lib/models/User.js');
+const Auction = require('../lib/models/Auction.js');
 
 const request = require('supertest');
 const app = require('../lib/app');
@@ -47,6 +48,26 @@ describe('auction routes', () => {
           description: 'Description for Camera Lens Auction', 
           quantity: 3, 
           endDate: expect.anything()
+        });
+      });
+  });
+
+  it('gets an auction by id via GET', () => {
+    return Auction.create({
+      user: user._id,
+      title: 'Camera Lens Auction', 
+      description: 'Description for Camera Lens Auction', 
+      quantity: 3, 
+      endDate: Date()
+    })
+      .then(auction=> request(app).get(`/api/v1/auctions/${auction._id}`))
+      .then(res => {
+        expect(res.body).toEqual({
+          user: user._id,
+          title: 'Camera Lens Auction', 
+          description: 'Description for Camera Lens Auction', 
+          quantity: 3, 
+          endDate: Date()
         });
       });
   });
