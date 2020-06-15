cf00115ce757a7edb5a69810218ba9d2fc6dc00d
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-06-15 15:29:07 -0700

    created post route and test passing for auction

---
+++ __tests__/auction.test.js
@@ -0,0 +1,53 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongod = new MongoMemoryServer();
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
+const User = require('../lib/models/User.js');
+
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('auction routes', () => {
+  beforeAll(async() => {
+    const uri = await mongod.getUri();
+    return connect(uri);
+  });
+  
+  let user;
+  beforeEach(async() => {
+    user = await User.create({
+      email: 'breeanntest@breeanntest.com',
+      passwordHash: 'password1234'
+    });
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+  
+  afterAll(async() => {
+    await mongoose.connection.close();
+    return mongod.stop();
+  });
+  
+  it('creates a new auction via POST', () => {
+    return request(app)
+      .post('/api/v1/auctions/')
+      .send({
+        user: user._id,
+        title: 'Camera Lens Auction', 
+        description: 'Description for Camera Lens Auction', 
+        quantity: 3, 
+        endDate: Date()
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          user: user.id,
+          title: 'Camera Lens Auction', 
+          description: 'Description for Camera Lens Auction', 
+          quantity: 3, 
+          endDate: expect.anything()
+        });
+      });
+  });
+});
